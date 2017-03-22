0055105ac227ff6e12caaf9676442ee63fea341e
vsippola <vsippola@uwo.ca>
2017-03-22 03:24:29 -0400

    Basic player movement added. Test case has block at (2,2)

---
+++ SudsyDemo/Assets/GameBoardEvent.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    public class GameBoardEvent
+    {
+
+        public GameBoardEvent() { }
+
+        public virtual Boolean doEvent(GameBoard gb)
+        {
+            return false;
+        }
+
+    }
+
+    public class PlayerMoveEvent : GameBoardEvent
+    {
+
+        public PlayerMoveEvent() { }
+
+    }
+
+    //move player up event
+    public class PlayerMoveEventUp : PlayerMoveEvent
+    {
+
+        public override Boolean doEvent(GameBoard gb)
+        {
+
+            ActorPlayer p = gb.player;
+
+            if (p.row <= 0) return false;
+
+            Tile tile = gb.board[p.row - 1][p.col];
+
+            return tile.MoveTo(gb.player);
+
+        }
+
+    }
+
+    public class PlayerMoveEventLeft : PlayerMoveEvent
+    {
+
+        public override Boolean doEvent(GameBoard gb)
+        {
+
+            ActorPlayer p = gb.player;
+
+            if (p.col <= 0) return false;
+
+            Tile tile = gb.board[p.row][p.col - 1];
+
+            return tile.MoveTo(gb.player);
+
+        }
+
+    }
+
+    public class PlayerMoveEventDown : PlayerMoveEvent
+    {
+
+        public override Boolean doEvent(GameBoard gb)
+        {
+
+            ActorPlayer p = gb.player;
+
+            if (p.row >= (gb.height - 1) ) return false;
+
+            Tile tile = gb.board[p.row + 1][p.col];
+
+            return tile.MoveTo(gb.player);
+
+        }
+
+    }
+
+    public class PlayerMoveEventRight : PlayerMoveEvent
+    {
+
+        public override Boolean doEvent(GameBoard gb)
+        {
+
+            ActorPlayer p = gb.player;
+
+            if (p.col >= (gb.width - 1) ) return false;
+
+            Tile tile = gb.board[p.row][p.col + 1];
+
+            return tile.MoveTo(gb.player);
+
+        }
+
+    }
+}
---
+++ SudsyDemo/Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Assets;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    GameBoard gb;
+
+    // Use this for initialization
+    void Start()
+    {
+        gb = gameObject.AddComponent<GameBoard>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w") || Input.GetKeyDown(KeyCode.UpArrow) )
+        {
+            print("w key was pressed");
+            gb.doGameEvent(new PlayerMoveEventUp());
+        }
+        else if (Input.GetKeyDown("a") || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            print("a key was pressed");
+            gb.doGameEvent(new PlayerMoveEventLeft());
+        }
+
+        else if (Input.GetKeyDown("s") || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("s key was pressed");
+            gb.doGameEvent(new PlayerMoveEventDown());
+        }
+
+        else if (Input.GetKeyDown("d") || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            print("d key was pressed");
+            gb.doGameEvent(new PlayerMoveEventRight());
+        }
+
+    }
+
+}
