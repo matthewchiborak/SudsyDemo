2be49bc5ad7527faf0abdb7a03908f3fbae4847a
vsippola <vsippola@uwo.ca>
2017-03-22 03:29:24 -0400

    Added basic player movement. Test board has barrier at (2,2)

---
+++ SudsyDemo/Assets/MoveBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+
+    //Maybe return a gamebehviour for updating/chaining?
+    
+    public class MoveBehavior
+    {
+
+        public virtual Boolean move(Actor actor, Tile block)
+        {
+
+            return false;
+
+        }
+
+    }
+
+    public class MoveBehaviorDeafult : MoveBehavior
+    {
+
+        public override Boolean move(Actor actor, Tile block)
+        {
+
+            actor.row = block.row;
+            actor.col = block.col;
+
+            return true;
+
+        }
+
+    }
+
+    public class MoveBehaviorBlock : MoveBehavior
+    {
+
+        public override Boolean move(Actor actor, Tile block)
+        {
+
+            return false;
+
+        }
+
+    }
+}
